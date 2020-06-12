f5b4da35d1ef89836d7032d5c48694b8716d8006
3e8 <info@3e8.ch>
2020-06-12 22:43:07 +0200

    add test route write

--- kbserver.js
+++ kbserver.js
@@ -11,6 +11,12 @@ app.use("/test", async (req, res)=>{
 	const fs = require("fs").promises
 	res.status(200).send(JSON.stringify(await fs.readdir("/data")))
 })
+app.use("/mkdir", async (req, res)=>{
+	const fs = require("fs").promises
+	await fs.mkdir("/data/2012")
+	await fs.writeFile("/data/2021/time.txt", ""+Date.now(), "utf-8")
+	res.status(200).send(JSON.stringify(await fs.readdir("/data")))
+})
 app.use("/admin", adminhandlers)
 app.use("/stplversions/1920/", expressStaticGzip("./import/1920/stpl_extractedVersions", staticOptions))
 app.use("/import", expressStaticGzip("./import/import_api", staticOptions))
