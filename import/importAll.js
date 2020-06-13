8c5c62733b222a9bc463511a9dc830c756fb8261
3e8 <info@3e8.ch>
2020-06-13 15:43:24 +0200

    open all routes

--- import/importAll.js
+++ import/importAll.js
@@ -14,15 +14,15 @@ const {compressDir} = require("./importhelpers/compressor")
 // const forceUpload = true;
 
 async function importAll(SJ) {
-	// await fetchStplInputs(SJ)
-	// //may be move DatenKB to correct /stpldata/year folder
-	// const {newFileFound} = await stplUpdate(SJ)
-	// const hasStplChanged = await stplExtract(SJ)
-	//
-	// //await compressDir("../../cacheSettingsTest")
-	// let {hasChanged, ...rest} = await fetchEventoApi(SJ)
-	// let haveFilesChanged = await handleEventoData(SJ)
-	// let d = await eventoMerge(SJ)
+	await fetchStplInputs(SJ)
+	//may be move DatenKB to correct /stpldata/year folder
+	const {newFileFound} = await stplUpdate(SJ)
+	const hasStplChanged = await stplExtract(SJ)
+
+	//await compressDir("../../cacheSettingsTest")
+	let {hasChanged, ...rest} = await fetchEventoApi(SJ)
+	let haveFilesChanged = await handleEventoData(SJ)
+	let d = await eventoMerge(SJ)
 	//
 	// await compressDir(PATHS.getStplRaw(SJ))
 	// await compressDir(PATHS.getEventoRaw(SJ))
