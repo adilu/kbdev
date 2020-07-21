a30074062175a7ea1aaed5ea6134e2d000beea5f
3e8 <info@3e8.ch>
2020-07-21 23:12:22 +0200

    init auth

--- kbserver.js
+++ kbserver.js
@@ -7,6 +7,7 @@ const cors = require("cors")
 const PATHS = require("./import/paths")
 
 let {adminhandlers} = require("./import/import_api/adminhandlers.js")
+let {auth2_handlers} = require("./auth/auth2_handlers.js")
 
 const corsOptions = {
 	origin: [/localhost/, /gymburgdorf/]
@@ -32,6 +33,7 @@ app.use("/import", expressStaticGzip("./import/import_api", staticOptions))
 app.use("/data", cors(corsOptions), expressStaticGzip(PATHS.root_data, staticOptions))
 app.use("/config", cors(corsOptions), expressStaticGzip("./config", staticOptions))
 app.use("/cacheSettingsTest", cors(corsOptions), expressStaticGzip("./cacheSettingsTest", staticOptions))
+app.use("/auth", cors(corsOptions), auth2_handlers)
 app.use("/", expressStaticGzip("./build", staticOptions))
 app.use("/", (req, res) => {res.send(`<DOCTYPE HTML><html><body><img src="https://image.freepik.com/free-vector/page-with-404-code-construction_89224-2833.jpg">
 	<div><a style="font-family: 'Segoe UI', sans-serif; font-size: 2em; color: #224;" href="https://www.gymburgdorf.ch">Zur Gymnasium Burgdorf Haupseite</a></div></div>	
