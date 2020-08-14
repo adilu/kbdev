634d2b28ef2483e4c9e1e84e1da1abf69bd5e8a7
3e8 <info@3e8.ch>
2020-08-14 19:54:07 +0200

    set encoding of gpp002.txt to latin1

--- import/_220_stplUpdate.js
+++ import/_220_stplUpdate.js
@@ -8,7 +8,7 @@ async function stplUpdate(SJ) {
 	let extracted = await readdir(PATHTOEXTRACTED)
 	let newest = extracted.filter(f=>f.endsWith(".txt")&&f.startsWith("extracted_")).sort().reverse()[0]
 	let newestContent = newest ? await readFile(PATHTOEXTRACTED + "/" + newest, "UTF-8") : ""
-	let candidateContent = await readFile(PATHS.getStplRawLatestCopy(SJ) + "/gpp002.txt", "UTF-8")
+	let candidateContent = await readFile(PATHS.getStplRawLatestCopy(SJ) + "/gpp002.txt", "latin1")
 	console.log(`Server content ready: reading ${candidateContent.split("\n").length} lines`)
 	if(candidateContent.length < 1000) throw new Error("Server file too small.")
 	const newFileFound = newestContent !== candidateContent
