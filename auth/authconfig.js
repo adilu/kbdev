c76bdf4df1a91d188003eeacde9a944bd116aea3
3e8 <info@3e8.ch>
2020-12-31 10:49:04 +0100

    update authconfig url

--- auth/authconfig.js
+++ auth/authconfig.js
@@ -1,11 +1,11 @@
 const os = require("os")
 
 const authconfig = {
-	MYURL: os.platform().includes("win") ? "http://localhost:11300/auth" : "https://gymburgdorf1.hidora.ch/auth",
+	MYURL: os.platform().includes("win") ? "http://localhost:11300/auth" : "https://gymburgdorf1.hidora.com/auth",
 	//https://hello.gymburgdorf.ch, https://auth.gymburgdorf.ch
 	entrypath: "/authtest",
 	loginpath: "/logintest",
-	callbackpath: "/redirecttest", //adapt on console.google.com > adrian.luethi@gymburgdorf.ch > auth2
+	callbackpath: "/redirecttest", //adapt on console.developers.google.com > adrian.luethi@gymburgdorf.ch > auth2
 }
 
 module.exports = authconfig
